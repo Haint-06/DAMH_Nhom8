9c6bf1eb9543849c7924a1b5ec37b1f5ae7d5753
Haint-06 <truonghai003email9@gmail.com>
2026-07-16 09:09:42 +0700

    Updated!

--- Database-W7/src/app.module.ts
+++ Database-W7/src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomsModule } from './rooms/rooms.module';
 import { BookingsModule } from './bookings/bookings.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 
 @Module({
-  imports: [RoomsModule, BookingsModule],
+  imports: [
+    RoomsModule,
+    BookingsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
